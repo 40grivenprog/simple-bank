cd94ca1f21ea34aff56ae3f959d51142f1702c89
filipenkaProg <Maksim.Filipenka@itechart-group.com>
2023-07-20 17:42:06 +0200

    21. Implement login user API that returns PASETO or JWT access token in Go

--- util/config.go
+++ util/config.go
@@ -1,19 +1,24 @@
 package util
 
-import "github.com/spf13/viper"
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
-	DBDriver      string `mapstructure:"DB_DRIVER"`
-	DBSource      string `mapstructure:"DB_SOURCE"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	DBDriver            string        `mapstructure:"DB_DRIVER"`
+	DBSource            string        `mapstructure:"DB_SOURCE"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
-
-func LoadConfig(path string)(config Config, err error) {
+func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
-	
+
 	viper.AutomaticEnv()
 
 	err = viper.ReadInConfig()
@@ -21,6 +26,6 @@ func LoadConfig(path string)(config Config, err error) {
 		return
 	}
 
-	err = viper.Unmarshal(&config )
+	err = viper.Unmarshal(&config)
 	return
 }
