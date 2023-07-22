cac88b99efab21bd945de6293266076100082250
filipenkaProg <Maksim.Filipenka@itechart-group.com>
2023-07-22 12:29:26 +0200

    29. Store & retrieve production secrets with AWS secrets manager

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 set -e
 
 echo "run db migration"
+source /app/app.env # sets variables like $DB_SOURCE
 /app/migrate -path /app/migration --database "$DB_SOURCE" --verbose up
 
 echo "start the app"
