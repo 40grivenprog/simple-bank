c8f2ab3d5d7009726ac026c0b03f8d5ab94bd150
filipenkaProg <Maksim.Filipenka@itechart-group.com>
2023-07-25 15:48:27 +0200

    43. gRPC Gateway: write code once, serve both gRPC & HTTP requests

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/40grivenprog/simple-bank/api"
 	db "github.com/40grivenprog/simple-bank/db/sqlc"
 	"github.com/40grivenprog/simple-bank/gapi"
 	"github.com/40grivenprog/simple-bank/pb"
 	"github.com/40grivenprog/simple-bank/util"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -27,7 +31,7 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
-
+	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 }
 
@@ -49,6 +53,47 @@ func runGrpcServer(config util.Config, store db.Store) {
 	}
 	log.Printf("start gRPC server on this address", listener.Addr().String())
 	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start grpc server", err)
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create grpc server", err)
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+
+	if err != nil {
+		log.Fatal("cannot register handler server", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HttpServerAddress)
+
+	if err != nil {
+		log.Fatal("cannot create listener", err)
+	}
+
+	log.Printf("start HTTP Gateway server on  address", listener.Addr().String())
+	err = http.Serve(listener, mux)
 	if err != nil {
 		log.Fatal("cannot start grpc server", err)
 	}
